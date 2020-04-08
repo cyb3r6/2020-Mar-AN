d32ff551cedb12b2670b7ad47ad4be45fbceef6e
nakisa <nakisa.donnelly@gmail.com>
2020-04-07 18:24:35 -0600

    Session 3 (cont.)
    - Github: creating repositories, pushing changes and pulling projects. 
    - Beginning scripting: understanding the Start() method. 
    - public vs private variables. 
    - Moving Liam's transform.position using a Transform and Vector3.

---
+++ 2020 March AN/Assets/Scripts/SimHandMove.cs
@@ -0,0 +1,21 @@
+﻿// Namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandMove : MonoBehaviour
+{
+    public Transform location;
+    public Vector3 otherLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = otherLocation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
