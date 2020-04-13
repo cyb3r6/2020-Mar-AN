564a5db8e80d3d9e41cb6c562152b846128a3dc1
nakisa <nakisa.donnelly@gmail.com>
2020-04-13 09:26:50 -0600

    Session 4
    - Using the transform.Translate() to move a gameobject in a given direction.
    - Understanding Time.deltaTime.
    - Using our Left Hand Rule to understand transform.Rotate().
    - Using Rigidbody.AddForce() to have Liam jump.
    - Using GetComponent<T>() to get a component of type T of the same gameobject our script is attached to.Using keyboard input to move.

--- 2020 March AN/Assets/Scripts/SimHandMove.cs
+++ 2020 March AN/Assets/Scripts/SimHandMove.cs
@@ -6,16 +6,83 @@ using UnityEngine;
 public class SimHandMove : MonoBehaviour
 {
     public Transform location;
-    public Vector3 otherLocation;
+    private Vector3 otherLocation = new Vector3(500f, 50f, 500f);
+
+    private Rigidbody liamRigidbody;
+    public float moveSpeed;
+    public float turnSpeed;
+    public float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = otherLocation;
+        liamRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        #region translational movement
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+            liamRigidbody.AddForce(Vector3.up * jumpForce);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
+        }
+
+
+        #endregion
+
+        #region rotation using keyboard
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * turnSpeed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * turnSpeed, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * turnSpeed, Space.Self);
+        }
+
+        #endregion
+
+        #region rotation using mouse
+
+        //transform.Rotate(Vector3.up );
+
+        #endregion
+    }
+
+    void Peter()
+    {
+        transform.position = otherLocation;
     }
 }
