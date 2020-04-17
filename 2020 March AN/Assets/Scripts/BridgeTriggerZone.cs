c134f667d7dc02a52cdea6dadec33f5809b32a28
nakisa <nakisa.donnelly@gmail.com>
2020-04-16 19:46:46 -0600

    Session 6
    - Creating a bridge rising animation. 
    - Understanding using parameters to trigger an animation state. 
    - Using an OnTriggerEnter(Collider other) to tigger a parameter on an animator controller.  
    - Checking for the tag of a gameobject i.e Liam is tagged as 'Player'. 
    - Instantiating a paintball prefab at a spawn point.

---
+++ 2020 March AN/Assets/Scripts/BridgeTriggerZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTriggerZone : MonoBehaviour
+{
+    public Animator bridgeAnim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("the object that entered the bridge triggerzone is: " + other.gameObject.name);
+        if(other.gameObject.tag == "Player")
+        {
+            bridgeAnim.SetTrigger("RaiseBridge");
+        }        
+    }
+}
---
+++ 2020 March AN/Assets/Scripts/ShootPaintball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPaintball : MonoBehaviour
+{
+    public GameObject paintballPrefab;
+    public Transform spawnPoint;    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(paintballPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
