f137e702e68493ef178b8ebbd30b8bfa7b110b16
nakisa <nakisa.donnelly@gmail.com>
2020-05-01 10:58:39 -0600

    Session 10
    - Using OnTriggerEnter/Exit methods to detect what we're touching.
    - Created our SimHandGrab using the parenting method. 
    - Created our VRGrab using a similar methodUsing a "flag" in our VRGrab script to call Grab() only once.
    - Snapped our heldObject into place on Grab().

---
+++ 2020 March AN/Assets/Scripts/SimHand/SimHandGrab.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandGrab : MonoBehaviour
+{
+    public GameObject collidingObject;  // what we're touching
+    public GameObject heldObject;       // what we're holding
+    public Transform snapPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collidingObject = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == collidingObject)
+        {
+            collidingObject = null;
+        }
+    }
+
+    void Update()
+    {
+        // check for input
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            // do the grabbing
+            if(collidingObject && collidingObject.GetComponent<Rigidbody>())
+            {
+                heldObject = collidingObject;
+                Grab();
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            // do the release
+            if (heldObject)
+            {
+                Release();
+            }
+
+        }
+    }
+
+    public void Grab()
+    {
+        heldObject.transform.SetParent(this.snapPosition);
+        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        heldObject.transform.localPosition = Vector3.zero;
+        heldObject.transform.rotation = snapPosition.rotation;
+    }
+
+    public void Release()
+    {
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.transform.SetParent(null);
+        heldObject = null;
+    }
+}
---
+++ 2020 March AN/Assets/Scripts/VR/VRGrab.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGrab : MonoBehaviour
+{
+    public GameObject collidingObject;  // what we're touching
+    public GameObject heldObject;       // what we're holding
+    public Transform snapPosition;
+
+    public bool gripHeld;
+
+    private VRInput controller;
+    
+    void Start()
+    {
+        controller = GetComponent<VRInput>();
+    }
+
+    
+    void Update()
+    {
+        if(controller.gripValue > 0.8f && gripHeld == false)
+        {
+            gripHeld = true;
+
+            // do the grabbing
+            if (collidingObject && collidingObject.GetComponent<Rigidbody>())
+            {
+                heldObject = collidingObject;
+                Grab();
+                
+            }
+        }
+        else if(controller.gripValue < 0.8f && gripHeld == true)
+        {
+            gripHeld = false;
+
+            // do the release
+            if (heldObject)
+            {
+                Release();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collidingObject = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == collidingObject)
+        {
+            collidingObject = null;
+        }
+    }
+
+    public void Grab()
+    {
+        Debug.Log("Grab is called");
+
+        heldObject.transform.SetParent(this.snapPosition);
+        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        heldObject.transform.localPosition = Vector3.zero;
+    }
+
+    public void Release()
+    {
+        Debug.Log("Release is called");
+
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.transform.SetParent(null);
+        heldObject = null;
+    }
+}
