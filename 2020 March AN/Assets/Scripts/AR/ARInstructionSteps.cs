66283f3a5704b5ccc67f923792325669bd317d8a
nakisa <nakisa.donnelly@gmail.com>
2020-05-19 19:35:18 -0600

    Session 15
    - Finished our physics based spring button. 
    - Started our Vuforia ARUtility app. 
    - Using a for(int i = 0; [i condition]; i++) loop to add child objects as steps.

---
+++ 2020 March AN/Assets/Scripts/AR/ARInstructionSteps.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARInstructionSteps : MonoBehaviour
+{
+    public GameObject mainMenuCanvas;
+    public GameObject initializeHeatCanvas;
+
+    // TODO: more gameobject canvas for instruciton steps!
+
+    private List<GameObject> steps = new List<GameObject>();
+    private int currentStep = 0;
+    private GameObject currentCanvas;
+
+    public void TurnOnHeatSequenceCanvas()
+    {
+        initializeHeatCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+        currentCanvas = initializeHeatCanvas;
+
+        // add the steps to our list!
+
+        for(int i = 0; i < initializeHeatCanvas.transform.childCount -1; i++)
+        {
+            steps.Add(initializeHeatCanvas.transform.GetChild(i).gameObject);
+            Debug.Log("The steps are " + steps[i]);
+        }
+    }
+}
---
+++ 2020 March AN/Assets/Scripts/SpringButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpringButton : MonoBehaviour
+{
+    public UnityEvent buttonPressedEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SpringButton")
+        {
+            buttonPressedEvent?.Invoke();
+        }
+    }
+}
