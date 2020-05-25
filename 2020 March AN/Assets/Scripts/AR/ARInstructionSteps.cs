ffc31fe86ee19221f9907eb0b084895073f3710d
nakisa <nakisa.donnelly@gmail.com>
2020-05-25 09:58:41 -0600

    Session 16
    - Finished our ARUtility app.
    - Using the ++ operand before using an integer. 
    - Playing a video clip on a cube or plane. 
    - Started creating Levers in VR.

--- 2020 March AN/Assets/Scripts/AR/ARInstructionSteps.cs
+++ 2020 March AN/Assets/Scripts/AR/ARInstructionSteps.cs
@@ -6,6 +6,7 @@ public class ARInstructionSteps : MonoBehaviour
 {
     public GameObject mainMenuCanvas;
     public GameObject initializeHeatCanvas;
+    public GameObject saltWaterCanvas;
 
     // TODO: more gameobject canvas for instruciton steps!
 
@@ -20,11 +21,62 @@ public class ARInstructionSteps : MonoBehaviour
         currentCanvas = initializeHeatCanvas;
 
         // add the steps to our list!
+        steps.Clear();
 
-        for(int i = 0; i < initializeHeatCanvas.transform.childCount -1; i++)
+        for(int i = 0; i < initializeHeatCanvas.transform.childCount - 2; i++)
         {
             steps.Add(initializeHeatCanvas.transform.GetChild(i).gameObject);
             Debug.Log("The steps are " + steps[i]);
         }
+    }
+
+    public void TurnOnSaltWaterCanvas()
+    {
+        saltWaterCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+        currentCanvas = saltWaterCanvas;
+
+        steps.Clear();
+
+        for (int i = 0; i < saltWaterCanvas.transform.childCount - 2; i++)
+        {
+            steps.Add(saltWaterCanvas.transform.GetChild(i).gameObject);
+            Debug.Log("The steps are " + steps[i]);
+        }
+    }
+
+    // TODO: add another set of instructions!
+    //       add sounds to buttons!
+
+    public void NextStep()
+    {
+        steps[currentStep].SetActive(false);
+
+        if(currentStep == steps.Count - 1)
+        {
+            currentStep = 0;
+            steps[currentStep].SetActive(true);
+            currentCanvas.SetActive(false);
+            mainMenuCanvas.SetActive(true);
+
+            return;
+        }
+
+        steps[++currentStep].SetActive(true);
+    }
+
+    public void PreviousStep()
+    {
+        steps[currentStep].SetActive(false);
+
+        if(currentStep <= 0)
+        {
+            currentStep = 0;
+            steps[currentStep].SetActive(true);
+
+            return;
+        }
+
+        steps[--currentStep].SetActive(true);
     }
 }
---
+++ 2020 March AN/Assets/Scripts/Lever.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public HingeJoint leverJoint;
+
+    public float NormalizedJointAngle()
+    {
+        float normalizedAngle = leverJoint.angle / leverJoint.limits.max;
+        return normalizedAngle;
+    }
+}
