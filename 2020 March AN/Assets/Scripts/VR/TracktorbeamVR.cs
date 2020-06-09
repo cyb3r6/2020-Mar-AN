f07a59b1778158584fd5582e280d99b0a1e89dc5
nakisa <nakisa.donnelly@gmail.com>
2020-06-09 13:49:41 -0600

    Session 20
    - Created a VR Tractor beam using Vector3.Lerp() and Vector3.MoveTowards(). 
    - Intro to VFX Graph.
    - Created a magical orb using 3 different particles: our trail particles, a single glowing particle in the middle and Perlin Noise particles conforming to a sphere.

---
+++ 2020 March AN/Assets/Scripts/VR/TracktorbeamVR.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TracktorbeamVR : GrabbableObjectVR
+{
+    public Transform startingPoint;
+    public float tractorBeamSpeed;
+    private LineRenderer tractorBeam;
+    private RaycastHit hit;
+    private Transform hitTransform;
+    private Rigidbody hitRigidbody;
+
+    
+    void Start()
+    {
+        tractorBeam = GetComponent<LineRenderer>();
+    }
+
+    
+    void Update()
+    {
+        if (isBeingHeld == true)
+        {
+            if (controller.triggerValue > 0.5f)
+            {
+                Interaction();
+            }
+            else
+            {
+                Drop();
+            }
+        }
+    }
+
+    public override void Interaction()
+    {
+        if(Physics.Raycast(startingPoint.position, transform.forward, out hit))
+        {
+            hitTransform = hit.transform;
+            tractorBeam.enabled = true;
+            tractorBeam.SetPosition(0, startingPoint.position);
+            tractorBeam.SetPosition(1, hit.point);
+
+            hitRigidbody = hitTransform.GetComponent<Rigidbody>();
+
+            if(hitRigidbody && !hitRigidbody.isKinematic)
+            {
+                hitTransform.position = Vector3.Lerp(hitTransform.position, startingPoint.position, Time.deltaTime);
+                //hitTransform.position = Vector3.MoveTowards(hitTransform.position, startingPoint.position, Time.deltaTime * tractorBeamSpeed);
+                hitRigidbody.useGravity = false;
+            }
+            else
+            {
+                hitTransform = null;
+            }
+        }
+        else
+        {
+            Drop();
+        }
+    }
+   
+    void Drop()
+    {
+        tractorBeam.enabled = false;
+        if (hitTransform)
+        {
+            hitRigidbody.useGravity = true;
+            hitTransform = null;
+            hitRigidbody = null;
+        }
+    }
+}
